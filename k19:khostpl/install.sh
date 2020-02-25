f2098168f43f3f9ac84f41c057f3a2301955f53f
jorge Pastor <jorge.pastor.rr@gmail.com>
2020-02-25 11:21:35 +0100

    ..

--- k19:khostpl/install.sh
+++ k19:khostpl/install.sh
@@ -12,7 +12,7 @@ done
 authconfig --enableshadow --enablelocauthorize \
    --enableldap \
    --enableldapauth \
-   --ldapserver='ldap.edt.org' \
+   --ldapserver='ldapserver.edt.org' \
    --ldapbase='dc=edt,dc=org' \
    --enablekrb5 --krb5kdc='kserver.edt.org' \
    --krb5adminserver='kserver.edt.org' --krb5realm='EDT.ORG' \
